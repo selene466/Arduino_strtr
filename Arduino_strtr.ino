2aa56311ba4e66d8981c64c0b8e7d1edd412e2b3
selene466 <selene.banderas.466@gmail.com>
2023-07-18 20:46:45 +0700

    initial commit

---
+++ Arduino_strtr.ino
@@ -0,0 +1,55 @@
+void setup() {
+  // Set serial baudrate
+  Serial.begin(9600);
+  // define originalChar & translateChar
+  String originalChar = "abcdefghijklmnopqrstuvwxyz",
+         translateChar = "zyxwvutsrqponmlkjihgfedcba",
+         // example string
+    originalString = "the quick brown fox jumps over the lazy dog",
+         // create var to save the result
+    translatedString1 = "",
+         translatedString2 = "";
+
+  // wipe serial screen
+  Serial.println("\n");
+  Serial.println("\n");
+  Serial.println("Example implementing php strtr() in Arduino C:\n");
+
+  // example translating char
+  // Output
+  // Original String #1: the quick brown fox jumps over the lazy dog
+  // Translated String #1: gsv jfrxp yildm ulc qfnkh levi gsv ozab wlt
+  translatedString1 = strtr(originalString, originalChar, translateChar);
+  Serial.print("Original String #1: ");
+  Serial.println(originalString);
+  Serial.print("Translated String #1: ");
+  Serial.println(translatedString1);
+  Serial.println("\n");
+
+  // example revert back translated char
+  // Output
+  // Original String #2: gsv jfrxp yildm ulc qfnkh levi gsv ozab wlt
+  // Translated String #2: the quick brown fox jumps over the lazy dog
+  translatedString2 = strtr(translatedString1, translateChar, originalChar);
+  Serial.print("Original String #2: ");
+  Serial.println(translatedString1);
+  Serial.print("Translated String #2: ");
+  Serial.println(translatedString2);
+}
+
+void loop() {}
+
+// function strtr()
+String strtr(String inputString, String inputFrom, String inputTo) {
+  String result = "";
+  for (int i = 0; i < inputString.length(); i++) {
+    String tmp = String(inputString[i]);
+    for (int j = 0; j < inputFrom.length(); j++) {
+      if (inputString[i] == inputFrom[j]) {
+        tmp.replace(inputFrom[j], inputTo[j]);
+      }
+    }
+    result += tmp;
+  }
+  return result;
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+Arduino strtr()
+===============
+
+Implementing php function strtr() in Arduino C
+
+## Why?
+
+The reason I created this repo because I can't find any of it on the internet.
+I hope this implementation can help you...
+
+## How to use?
+
+Example code & usage is inside "Arduino_strtr.ino".
